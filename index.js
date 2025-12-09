e31e6dcdb717e1e73b20851055b611102f8826bd
alinakotlovets <a.kotlovets@lntu.edu.ua>
2025-12-09 17:45:09 +0200

    make adapt for mobile and tablet

--- index.js
+++ index.js
@@ -11,3 +11,42 @@ window.addEventListener('scroll', function () {
     }
     prevScrollPos = currentScrollPos;
 });
+
+
+const burgerBtn = document.querySelector('.burger-menu-button');
+
+function showBurgerMenu() {
+    if (window.innerWidth < 768) {
+        burgerBtn.style.display = 'block';
+        document.querySelector('nav').style.display = 'none';
+    } else {
+        burgerBtn.style.display = 'none';
+        document.querySelector('nav').style.display = 'flex';
+    }
+}
+
+showBurgerMenu();
+window.addEventListener('resize', function () {
+    showBurgerMenu();
+});
+
+
+burgerBtn.addEventListener('click', function () {
+    document.querySelector('nav').style.display = 'flex';
+    document.querySelector('.close-mobile-menu-btn').style.display = 'block';
+    document.querySelector('body').classList.add("hidden-overflow");
+})
+
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function () {
+        document.querySelector('nav').style.display = 'none';
+        document.querySelector('body').classList.remove("hidden-overflow");
+        document.querySelector('.close-mobile-menu-btn').style.display = 'none';
+    })
+})
+
+document.querySelector('.close-mobile-menu-btn').addEventListener('click', function () {
+    document.querySelector('nav').style.display = 'none';
+    document.querySelector('body').classList.remove("hidden-overflow");
+    document.querySelector('.close-mobile-menu-btn').style.display = 'none';
+})
